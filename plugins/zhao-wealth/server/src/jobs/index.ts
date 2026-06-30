6aca43f7daf045cdb0985526986aece6f9aaa463
zhao yitao <johocn@163.com>
2026-06-30 11:22:29 +0800

    feat(zhao-wealth): register risk-metric-job in jobs index

--- plugins/zhao-wealth/server/src/jobs/index.ts
+++ plugins/zhao-wealth/server/src/jobs/index.ts
@@ -3,9 +3,11 @@
 import { setupQueues } from './queue-setup';
 import { registerCollectJobs } from './collect-job';
 import { registerCalculateJobs } from './calculate-job';
+import { registerRiskMetricJobs } from './risk-metric-job';
 
 export default ({ strapi }) => {
   setupQueues(strapi);
   registerCollectJobs(strapi);
   registerCalculateJobs(strapi);
+  registerRiskMetricJobs(strapi);
 };
