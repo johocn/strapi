8f747afb9b933f686165ae4851b77abcefb7d183
zhao yitao <johocn@163.com>
2026-07-04 16:25:04 +0800

    feat(zhao-tag): 新增 tag-group content-type

--- plugins/zhao-tag/server/src/content-types/index.ts
+++ plugins/zhao-tag/server/src/content-types/index.ts
@@ -1,9 +1,11 @@
 import tagSchema from "./tag/schema.json";
 import knowledgePointSchema from "./knowledge-point/schema.json";
 import tagIndexSchema from "./tag-index/schema.json";
+import tagGroupSchema from "./tag-group/schema.json";
 
 export default {
   tag: { schema: tagSchema },
   "knowledge-point": { schema: knowledgePointSchema },
   "tag-index": { schema: tagIndexSchema },
+  "tag-group": { schema: tagGroupSchema },
 };
