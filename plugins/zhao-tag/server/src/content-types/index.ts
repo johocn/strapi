7628d4f75baab5db897d9f82c08974567d7c0f4c
zhao yitao <johocn@163.com>
2026-07-04 14:21:03 +0800

    feat(zhao-tag): 新增 tag-index content-type

--- plugins/zhao-tag/server/src/content-types/index.ts
+++ plugins/zhao-tag/server/src/content-types/index.ts
@@ -1,7 +1,9 @@
 import tagSchema from "./tag/schema.json";
 import knowledgePointSchema from "./knowledge-point/schema.json";
+import tagIndexSchema from "./tag-index/schema.json";
 
 export default {
   tag: { schema: tagSchema },
   "knowledge-point": { schema: knowledgePointSchema },
+  "tag-index": { schema: tagIndexSchema },
 };
