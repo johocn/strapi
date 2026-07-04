baf85394852c0bc60809d8a19061c9a28e690e35
zhao yitao <johocn@163.com>
2026-07-04 14:28:39 +0800

    feat(zhao-tag): 注册 controllers/services/routes

--- plugins/zhao-tag/server/src/index.ts
+++ plugins/zhao-tag/server/src/index.ts
@@ -1,8 +1,14 @@
 import contentTypes from "./content-types";
+import controllers from "./controllers";
+import services from "./services";
+import routes from "./routes";
 
 export default {
   register() {},
   bootstrap() {},
   destroy() {},
   contentTypes,
+  controllers,
+  services,
+  routes,
 };
