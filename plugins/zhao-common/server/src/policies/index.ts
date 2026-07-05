e6c2d4ffd986645a4ea3bb3891d26396997e67f7
zhao yitao <johocn@163.com>
2026-07-06 00:18:54 +0800

    feat(zhao-common): register resolve-channel-scope policy

--- plugins/zhao-common/server/src/policies/index.ts
+++ plugins/zhao-common/server/src/policies/index.ts
@@ -1,7 +1,9 @@
 import hasTenantAccessLoose from "./has-tenant-access-loose";
 import hasTenantAccessStrict from "./has-tenant-access-strict";
+import resolveChannelScope from "./resolve-channel-scope";
 
 export default {
   "has-tenant-access-loose": hasTenantAccessLoose,
   "has-tenant-access-strict": hasTenantAccessStrict,
+  "resolve-channel-scope": resolveChannelScope,
 };
